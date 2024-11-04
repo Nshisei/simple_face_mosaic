bfc59667457be201f4b9e537166557116e8053b5
Nshisei <n.shisei1215@gmail.com>
2024-11-04 16:04:37 +0900

    face mesh

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ import av
 import os
 
 from face_cover import overlay_image_alpha
+import os
+import sys
+current_dir = os.path.dirname(__file__)
+ar_dir = os.path.join(current_dir, 'AR')
+sys.path.append(ar_dir)
+from apply_filter import apply_face_mask, getLandmarks
 
 def face_detection_using_mediapipe():
     min_detection_confidence = st.slider('Min Detection Confidence', min_value=0.0, max_value=1.0, value=0.5, step=0.1)
@@ -48,37 +54,8 @@ def face_mesh_using_mediapipe():
     min_detection_confidence = st.slider('Min Detection Confidence', min_value=0.0, max_value=1.0, value=0.5, step=0.1)
 
     def callback(frame):
-        mp_face_detection = mp.solutions.face_detection
-        mp_face_mesh = mp.solutions.face_mesh
-        mp_drawing = mp.solutions.drawing_utils
-        mp_drawing_styles = mp.solutions.drawing_styles
-        drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
-
-        image = frame.to_ndarray(format="bgr24")
-        
-        # FaceDetectionとFaceMeshを使用
-        with mp_face_mesh.FaceMesh(static_image_mode=False, max_num_faces=1, min_detection_confidence=min_detection_confidence) as face_mesh:
-
-            image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            results_mesh = face_mesh.process(image_rgb)
-
-            if results_mesh.multi_face_landmarks:
-                for face_landmarks in results_mesh.multi_face_landmarks:
-                    # それぞれの接続を描画
-                    mp_drawing.draw_landmarks(
-                        image=image,
-                        landmark_list=face_landmarks,
-                        connections=mp_face_mesh.FACEMESH_TESSELATION,
-                        landmark_drawing_spec=drawing_spec,
-                        connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_tesselation_style()
-                    )
-                    mp_drawing.draw_landmarks(
-                        image=image,
-                        landmark_list=face_landmarks,
-                        connections=mp_face_mesh.FACEMESH_CONTOURS,
-                        landmark_drawing_spec=drawing_spec,
-                        connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_contours_style()
-                    )
+        points2 = getLandmarks(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), min_detection_confidence=0.3)
+        image = apply_face_mask(frame, points2)
 
         return av.VideoFrame.from_ndarray(image, format="bgr24")
 
